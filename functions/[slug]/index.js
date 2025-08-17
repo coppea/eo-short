adf35fa2bc55ae30978f26bcf970b95d3c1a8b04
coppea <86509331+coppea@users.noreply.github.com>
2025-08-17 17:08:23 +0800

    Update index.js

--- functions/[slug]/index.js
+++ functions/[slug]/index.js
@@ -398,7 +398,7 @@ export async function onRequest({ request, params, env }) {
   }
 
   if (!slug || slug === 'favicon.ico') {
-    return new Response(indexHtml, { headers: { 'Content-Type': 'text/html; charset=utf-8' } });
+    return new Response(siteHtml, { headers: { 'Content-Type': 'text/html; charset=utf-8' } });
   }
     if (!slug || slug === coppea) {
     return new Response(indexHtml, { headers: { 'Content-Type': 'text/html; charset=utf-8' } });
